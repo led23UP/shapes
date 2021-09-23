ca43d06d5982f21e9c5e6f7fd9609bd3a70f8ecf
David <led23@up.edu>
2021-09-23 15:35:26 -0700

    Initial commit

---
+++ app/src/main/java/com/example/shapes/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.shapes;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+}
