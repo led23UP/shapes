bae8ffc247c56feaeb308abe179bf51df7c83436
David <led23@up.edu>
2021-09-27 16:32:28 -0700

    Implemented win condition (checkWin()), resolved issues with check displaying text when it should be empty and the crashing issue

--- app/src/main/java/com/example/shapes/MainActivity.java
+++ app/src/main/java/com/example/shapes/MainActivity.java
@@ -8,21 +8,26 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.Toast;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private Button [] buttons = new Button[17];
     private Button resetGame;
-    private int[] tiles ={1,2,3,4,5,6,7,8,9,10,11,12,13,14,16,17};
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+
+        //loops through button array and "initialize" them to be ready to be clicked
         for(int i = 1; i < buttons.length;i++)
         {
             String buttonID = "square" + i;
@@ -32,35 +37,50 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             buttons[i].setBackgroundColor(Color.GRAY);
 
         }
+
+        Button resetButton = (Button) findViewById(R.id.resetButton);
+        resetButton.setOnClickListener(this);
         buttons[16].setBackgroundColor(Color.TRANSPARENT);
-        generate15();
-        Log.i("text","oncreate");
+
+        //generate15();
+
 
     }
 
     public void generate15()
     {
-        Random rand = new Random();
-        int count= 1;
-        int n = 15;
-        while(count!=14)
+
+        View view = this.getWindow().getDecorView();
+        view.setBackgroundColor(Color.BLACK);
+        /*
+        HashSet<Integer> used = new HashSet<Integer>();
+        for(int i = 1; i < buttons.length-1;i++)
         {
 
 
-            count++;
+                int add = (int)(Math.random() *15+1); //this is the int we are adding
+                while (used.contains(add)) { //while we have already used the number
+                    add = (int) (Math.random() * 15 +1); //generate a new one because it's already used
+                }
+                //by this time, add will be unique
+                used.add(add);
+                //result[i] = add;
+
+            buttons[i].setText(String.valueOf(add));
         }
-        for(int i = 1; i < buttons.length;i++)
-        {
-            int randomNum = rand.nextInt(17-i);
-            buttons[i].setText(String.valueOf(randomNum));
-        }
+        */
+
 
     }
 
 
     @Override
     public void onClick(View v) {
+        //checks which button is pressed
         switch(v.getId()) {
+            case R.id.resetButton:
+                generate15();
+                break;
             case R.id.square1:
                 check(1);
                 break;
@@ -105,7 +125,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 break;
             case R.id.square15:
                 check(15);
-                //Log.i("Text","check50");
                 break;
             case R.id.square16:
                 check(16);
@@ -116,43 +135,54 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     public void check(int square_num)
     {
-        Log.i("Text","first");
 
-
+            // RIGHT
+            //checks the tags of the button pressed and see if adjacent button
+            //is an "invisible". "invisible" is determined by the tag "16"
             if (square_num +1 < 17 && String.valueOf(buttons[square_num + 1].getTag()).equals("16")) {
-                Log.i("Text", "1");
+
+                //change adjacent button to match original button pressed
                 buttons[square_num + 1].setText(buttons[square_num].getText());
                 buttons[square_num +1 ].setTag(buttons[square_num].getTag());
                 buttons[square_num + 1].setBackgroundColor(Color.GRAY);
 
+                //make original button pressed invisible and carry the invisible values
                 buttons[square_num].setBackgroundColor(Color.TRANSPARENT);
                 buttons[square_num].setTag("16");
+                buttons[square_num].setText("");
+                checkWin();
                 return;
             }
+            //LEFT
             if (square_num-1 > 0 &&String.valueOf(buttons[square_num - 1].getTag()).equals("16")) {
-                Log.i("Text", "2");
+
                 buttons[square_num - 1].setText(buttons[square_num].getText());
                 buttons[square_num -1 ].setTag(buttons[square_num].getTag());
                 buttons[square_num - 1].setBackgroundColor(Color.GRAY);
 
                 buttons[square_num].setBackgroundColor(Color.TRANSPARENT);
                 buttons[square_num].setTag("16");
+                buttons[square_num].setText("");
+                checkWin();
                 return;
 
             }
-
+                //BOTTOM
                 if (square_num +4 < 17 && String.valueOf(buttons[square_num + 4].getTag()).equals("16")) {
-                    Log.i("Text", "3");
+
                     buttons[square_num + 4].setText(buttons[square_num].getText());
-                    buttons[square_num +4 ].setTag(buttons[square_num].getTag());
+                    buttons[square_num + 4].setTag(buttons[square_num].getTag());
                     buttons[square_num + 4].setBackgroundColor(Color.GRAY);
 
                     buttons[square_num].setBackgroundColor(Color.TRANSPARENT);
                     buttons[square_num].setTag("16");
+                    buttons[square_num].setText("");
+                    checkWin();
                     return;
 
                 }
-        if (square_num -4 > -1 && String.valueOf(buttons[square_num - 4].getTag()).equals("16")) {
+        //TOP
+        if (square_num -4 > 0 && String.valueOf(buttons[square_num - 4].getTag()).equals("16")) {
             Log.i("Text", "4");
             buttons[square_num - 4].setText(buttons[square_num].getText());
             buttons[square_num -4 ].setTag(buttons[square_num].getTag());
@@ -160,9 +190,41 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
             buttons[square_num].setBackgroundColor(Color.TRANSPARENT);
             buttons[square_num].setTag("16");
+            buttons[square_num].setText("");
+            checkWin();
             return;
 
         }
+
+
+    }
+
+    public void checkWin()
+    {
+        //loops through the button array
+        for(int i = 1; i < buttons.length-2;i++)
+        {
+
+            if(String.valueOf(buttons[i].getText()).equals("") ||
+                    String.valueOf(buttons[i+1].getText()).equals(""))
+            {
+                return;
+            }
+
+            if(! (Integer.parseInt(String.valueOf(buttons[i].getText())) +1 ==
+                    Integer.parseInt(String.valueOf(buttons[i+1].getText())))) {
+                return;
+            }
+
+        }
+            //win notification
+            View view = this.getWindow().getDecorView();
+            view.setBackgroundColor(Color.GREEN);
+
+            Log.i("Text","you win!");
+            Toast.makeText(getApplicationContext(),"You won!",Toast.LENGTH_SHORT).show();
+
+
 
 
     }
